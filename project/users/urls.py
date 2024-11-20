fb56ad8bcb9c885e1923a5cb241e388f338f90ef
dikay <dikiy@gmai.com>
2024-11-20 14:56:24 +0300

    add - jwt токен urls

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import RegisterView, ChangePasswordView
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('change-password/', ChangePasswordView.as_view(),name='change_password'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
