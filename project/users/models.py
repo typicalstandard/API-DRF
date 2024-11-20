5664bc1949da92ea71a4bc4d863562118226373f
dikay <dikiy@gmai.com>
2024-11-20 14:06:04 +0300

    fix - конфликт имен

--- project/users/models.py
+++ project/users/models.py
@@ -3,4 +3,19 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        related_name='customuser_user_permissions'  # Добавляем related_name здесь
+    )
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        related_name='customuser_groups'  # Добавляем related_name здесь
+    )
 
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
