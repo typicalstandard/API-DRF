9f8de519036f0bd70effc950dc4ed9498262a0cd
dikay <dikiy@gmai.com>
2024-11-21 02:03:41 +0300

    add -  LinkViewSet

--- project/users/views.py
+++ project/users/views.py
@@ -1,4 +1,12 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.utils import timezone
+from rest_framework import viewsets
+from .models import Link
+from .serializers import LinkSerializer
+from rest_framework.permissions import IsAuthenticated
 
+class LinkViewSet(viewsets.ModelViewSet):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
