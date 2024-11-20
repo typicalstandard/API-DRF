e191759e941e6ac34cb03a121efda6f93d2bcc50
dikay <dikiy@gmai.com>
2024-11-20 13:53:33 +0300

    add - модель юзера

--- project/users/models.py
+++ project/users/models.py
@@ -1,9 +1,6 @@
-# users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
 
-    def __str__(self):
-        return self.email
