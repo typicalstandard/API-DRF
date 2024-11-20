f9fe1bebf0de71d199a1a2e8b7dfaa112691b58e
dikay <dikiy@gmai.com>
2024-11-21 02:03:00 +0300

    fix - перенес все url с юхером в главный url и добавил обработчик ссылок

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,12 +1,9 @@
-# link_manager/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path('api/v1/drf-auth/',include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
-    path('api/auth/', include('djoser.urls')),
+    path('api/v1/', include('users.urls')),
+    path('api/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
 ]
--- project/users/urls.py
+++ project/users/urls.py
@@ -1,11 +1,10 @@
-from .views import RegisterView, ChangePasswordView, LoginView, LoginUser
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import LinkViewSet
+
+router = DefaultRouter()
+router.register(r'links', LinkViewSet, basename='link')
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('change-password/', ChangePasswordView.as_view(),name='change_password'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
-
