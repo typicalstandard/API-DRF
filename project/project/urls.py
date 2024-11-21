d976304affacc3ab1b11f6214739b83b9710edf8
dikay <dikiy@gmai.com>
2024-11-21 13:42:15 +0300

    add - документация swagger

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path, include,re_path
+from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+# Swagger Schema View
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API Documentation for managing users, links, and collections.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@api.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('users.urls')),
-    path('api/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.jwt')),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- project/users/urls.py
+++ project/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LinkViewSet,CollectionViewSet
+from .views import LinkViewSet, CollectionViewSet
 
 router = DefaultRouter()
 router.register(r'links', LinkViewSet, basename='link')
