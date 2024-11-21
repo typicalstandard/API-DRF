f7c8fd0819a317b3def9481aa30989fa93e8e2d3
dikay <dikiy@gmai.com>
2024-11-21 03:01:41 +0300

    update  - добавил в link связь многие ко многим

--- project/users/models.py
+++ project/users/models.py
@@ -32,6 +32,7 @@ class Link(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='links')
+    collections = models.ManyToManyField('Collection', related_name='links')
 
     def __str__(self):
         return self.title or self.url
