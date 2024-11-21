cb55d7c8d94f81915b331d0682734edd46525642
dikay <dikiy@gmai.com>
2024-11-21 03:05:40 +0300

    add - crud коллекциий

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LinkViewSet
+from .views import LinkViewSet,CollectionViewSet
 
 router = DefaultRouter()
 router.register(r'links', LinkViewSet, basename='link')
+router.register(r'collections', CollectionViewSet, basename='collection')
 
 urlpatterns = [
     path('', include(router.urls)),
